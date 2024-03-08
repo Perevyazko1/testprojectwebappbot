afc8c2f2cbbec92242fab9acbd6c55f2a36b8ed8
Perevyazko <a.perevyazko@gmail.com>
2024-03-08 17:07:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const app = window.Telegram?.WebApp;
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {app.initDataUnsafe?.user?.username}
         </a>
       </header>
     </div>
